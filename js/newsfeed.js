48b658383124ac600f13236633bb6da6d0bc8dfd
Popey Gilbert <popey@debenclipper.com>
2015-11-24 21:33:41 +0000

    xml to json and edit index.  Also, completely js newsfeed

--- js/newsfeed.js
+++ js/newsfeed.js
@@ -9,6 +9,8 @@ function getURL(){
     //console.log(json)
     var items = json["query"]["results"]["item"];
     //console.log(items);
+    $( ".newsfeed" ).append('<section class="card" id="newsfeedinner">');
+    $( ".newsfeed" ).append('</section>');
     for(i=0;i<3;i++){
       console.groupCollapsed("News Item: " + items[i]['title']['#text']);
       console.log(items[i]['title']['#text']);
@@ -20,47 +22,12 @@ function getURL(){
   })
   .fail(function() {getURL();});
 }
-// Changes XML to JSON - Noone likes XML, credit to David Walsh.
-function xmlToJson(xml) {
-	
-	// Create the return object
-	var obj = {};
-	if (xml.nodeType == 1) { // element
-		// do attributes
-		if (xml.attributes.length > 0) {
-		obj["@attributes"] = {};
-			for (var j = 0; j < xml.attributes.length; j++) {
-				var attribute = xml.attributes.item(j);
-				obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
-			}
-		}
-	} else if (xml.nodeType == 3) { // text
-		obj = xml.nodeValue;
-	}
-	// do children
-	if (xml.hasChildNodes()) {
-		for(var i = 0; i < xml.childNodes.length; i++) {
-			var item = xml.childNodes.item(i);
-			var nodeName = item.nodeName;
-			if (typeof(obj[nodeName]) == "undefined") {
-				obj[nodeName] = xmlToJson(item);
-			} else {
-				if (typeof(obj[nodeName].push) == "undefined") {
-					var old = obj[nodeName];
-					obj[nodeName] = [];
-					obj[nodeName].push(old);
-				}
-				obj[nodeName].push(xmlToJson(item));
-			}
-		}
-	}
-	return obj;
-};
+
 function appendToDiv(title, description, link){
   var firstWord = title.split(" ")[0];
   var otherWord = title.substr(title.indexOf(" ") + 1);
   var inner = "<a style='text-decoration: none;' href='" + link + "'><h1><strong>" + firstWord + "</strong> " + otherWord +"</h1><h2>" + description + "</h2></a>";
-  $( ".newsfeed" ).append( inner );
+  $( "#newsfeedinner" ).append( inner );
 }
 getURL();
 //http://feeds.bbci.co.uk/news/world/rss.xml?edition=uk
