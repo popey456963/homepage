b51def25e72260118209363d2b15f1087cb99879
Popey Gilbert <popey@debenclipper.com>
2015-11-24 21:02:21 +0000

    Clean up passwords, index and periodic

--- js/typelinks.js
+++ js/typelinks.js
@@ -31,5 +31,5 @@ cheet ('c 9', function() {
 });
 
 cheet ('p e r i o d i c', function() {
-    proceed("periodic");
+	document.write("<iframe src=\"./templates/periodic.html\" style=\"position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;\">Your browser doesn't support iframes</iframe>");
 });
